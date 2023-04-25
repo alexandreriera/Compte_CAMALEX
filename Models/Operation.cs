02ce217aa8706e36fc1bd6690716f837d8baee70
ALEXANDRE RIERA <riera968@hotmail.com>
2023-04-25 11:16:12 +0200

    ADD Class Operation dans un dossier Models

---
+++ Models/Operation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Operation
+{
+	private string type;
+	private double montant;
+
+	public Operation()
+	{
+	}
+	public Operation(string typeOperation, double montantOperation)
+	{
+		this.type = typeOperation;
+		this.montant = montantOperation;
+	}
+}
