1f4756bcf4b33cf21df66d88efb166d07dc0d080
ALEXANDRE RIERA <riera968@hotmail.com>
2023-04-25 08:41:25 +0200

    Delete class CompteBancaire de MainWindow + Delete button dans la vue

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -9,56 +9,11 @@ namespace Compte_CAMALEX
     /// </summary>
     public partial class MainWindow : Window
     {
-        private double solde;
-        private string devise;
-
+       
         public MainWindow()
         {
-        InitializeComponent();
+            InitializeComponent();
 
         }
-
-        private void Show_Debit_Click(object sender, RoutedEventArgs e)
-        {
-            MyPopup.IsOpen = true;
-        }
-
-        private void Hide_Debit_Click(object sender, RoutedEventArgs e)
-        {
-            MyPopup.IsOpen = false;
-        }
-
-
-        public void CompteBancaire(double soldeInitial, string devise)
-        {
-            this.solde = soldeInitial;
-            this.devise = " €";
-        }
-        public override string ToString()
-        {
-            return "Le solde de votre compte est de " + solde + " " + devise;
-        }
-
-        public double GetSolde()
-        {
-            return solde;
-        }
-
-        public double Debiter(double montant)
-        {
-            return solde -= montant;
-        }
-        public double Crediter(double montant)
-        {
-            return solde += montant;
-        }
-
-    }
-
-
-    public class CompteBancaire
-    {
-        
-
     }
 }
